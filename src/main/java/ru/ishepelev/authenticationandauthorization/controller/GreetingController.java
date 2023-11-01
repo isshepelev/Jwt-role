38ba0e742a4cb13ff67629e87347c3a9c27f7ed7
Ilya <isshepelev2004@gmail.com>
2023-11-01 22:05:16 +0300

    delete import

--- src/main/java/ru/ishepelev/authenticationandauthorization/controller/GreetingController.java
+++ src/main/java/ru/ishepelev/authenticationandauthorization/controller/GreetingController.java
@@ -4,8 +4,6 @@ package ru.ishepelev.authenticationandauthorization.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-import ru.ishepelev.authenticationandauthorization.domain.Auth;
-import ru.ishepelev.authenticationandauthorization.domain.Registration;
 import ru.ishepelev.authenticationandauthorization.service.UserService;
 
 @RestController
