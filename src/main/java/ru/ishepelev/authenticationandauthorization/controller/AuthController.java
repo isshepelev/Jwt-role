9eaf1463b5608884317e4774d6f591d540820c0a
Ilya <isshepelev2004@gmail.com>
2023-11-06 23:48:30 +0300

    Reformat code

--- src/main/java/ru/ishepelev/authenticationandauthorization/controller/AuthController.java
+++ src/main/java/ru/ishepelev/authenticationandauthorization/controller/AuthController.java
@@ -1,24 +1,13 @@
 package ru.ishepelev.authenticationandauthorization.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import ru.ishepelev.authenticationandauthorization.domain.User;
 import ru.ishepelev.authenticationandauthorization.dto.JwtRequest;
-import ru.ishepelev.authenticationandauthorization.dto.JwtResponse;
 import ru.ishepelev.authenticationandauthorization.dto.RegistrationUserDto;
-import ru.ishepelev.authenticationandauthorization.exception.AppError;
-import ru.ishepelev.authenticationandauthorization.service.UserService;
 import ru.ishepelev.authenticationandauthorization.service.impl.AuthService;
-import ru.ishepelev.authenticationandauthorization.utils.JwtUtils;
 
 import java.util.List;
 
