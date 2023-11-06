278ea35001d9afc77b651f99f734e99f45f07d20
Ilya <isshepelev2004@gmail.com>
2023-11-06 23:48:40 +0300

    Reformat code

--- src/main/java/ru/ishepelev/authenticationandauthorization/dto/UserDto.java
+++ src/main/java/ru/ishepelev/authenticationandauthorization/dto/UserDto.java
@@ -1,12 +1,8 @@
 package ru.ishepelev.authenticationandauthorization.dto;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import ru.ishepelev.authenticationandauthorization.domain.Role;
 
-import javax.persistence.*;
-import java.util.Collection;
 
 @Data
 @AllArgsConstructor
